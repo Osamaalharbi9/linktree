fb3b69416ec7bd9361ac74dc0742981c776fc44d
Osamaalharbi9 <osamaalharbi.9900@gmail.com>
2023-09-15 16:14:34 +0300

    third

--- linktree/lib/introduction_page/introduction_page.dart
+++ linktree/lib/introduction_page/introduction_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Introductionpage extends StatelessWidget {
   const Introductionpage({super.key});
@@ -18,17 +19,84 @@ class Introductionpage extends StatelessWidget {
                 Color.fromARGB(255, 206, 191, 218),
               ])),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            //mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.asset(
-                'assets/images/Comp 2_10.png',
-                height: 200,
-                width: 200,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/Comp 2_10.png',
+                    height: 200,
+                    width: 200,
+                  ),
+                ],
               ),
-              Image.asset('assets/images/profile_card.png'),
-              const Text('Your Pathway to grow'),
-              ElevatedButton(
-                  onPressed: () {}, child: const Text('Get Started!'))
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                      height: 360,
+                      width: 400,
+                      child: Image.asset(
+                        'assets/images/profile_card1.png',
+                        fit: BoxFit.fitHeight,
+                      )),
+                ],
+              ),
+              Row(mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Your Pathway ',
+                    style: GoogleFonts.inter(
+                        fontSize: 40,
+                        color: const Color.fromARGB(
+                          255,
+                          86,
+                          54,
+                          105,
+                        ),
+                        fontWeight: FontWeight.w500),
+                  ),
+                ],
+                
+              ),Row(
+                
+                children: [
+                  Text(
+                    'to grow ',
+                    style: GoogleFonts.inter(
+                        fontSize: 40,
+                        color: const Color.fromARGB(
+                          255,
+                          86,
+                          54,
+                          105,
+                        ),
+                        fontWeight: FontWeight.w500),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(60),
+                    child: Container(
+                      color: Colors.black,
+                      height: 60,
+                      width: 300,
+                      child: OutlinedButton(style: OutlinedButton.styleFrom(foregroundColor: Color.fromARGB(255, 121, 121, 121)),
+                          onPressed: () {},
+                          child: Text(
+                            'Get Started!',
+                            style: GoogleFonts.inter(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w700),
+                          )),
+                    ),
+                  ),
+                ],
+              )
             ],
           ),
         ),
