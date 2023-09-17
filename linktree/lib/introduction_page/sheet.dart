86eaca0805f5db5aae2290dfc75b7f4d0b7f8f8c
Osamaalharbi9 <osamaalharbi.9900@gmail.com>
2023-09-17 12:56:19 +0300

    textfield

--- linktree/lib/introduction_page/sheet.dart
+++ linktree/lib/introduction_page/sheet.dart
@@ -9,6 +9,7 @@ class Sheet extends StatefulWidget {
 }
 
 class _SheetState extends State<Sheet> {
+  TextEditingController name = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return OutlinedButton(
@@ -25,11 +26,76 @@ class _SheetState extends State<Sheet> {
   }
 
   Future _desplayBottomSheet(BuildContext context) {
-    return showModalBottomSheet(shape:const  RoundedRectangleBorder(borderRadius: BorderRadius.vertical(top:  Radius.circular(40))),
+    return showModalBottomSheet(
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(top: Radius.circular(40))),
         context: context,
-        builder: (context) => Container(
+        builder: (context) => SizedBox(
               height: 500,
-              child:const Column(children: [],),
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    'Sign up',
+                    style: GoogleFonts.inter(
+                        fontSize: 25,
+                        color: const Color.fromARGB(255, 0, 0, 0),
+                        fontWeight: FontWeight.w600),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30),
+                    child: TextField(
+                        controller: name,
+                        decoration: InputDecoration(
+                          suffixIcon: const Row(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 10),
+                                child: Icon(
+                                  Icons.person,
+                                ),
+                              ),
+                              Text('Enter your name ')
+                            ],
+                          ),
+                          filled: true,
+                          fillColor: const Color.fromARGB(255, 215, 215, 215),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20)),
+                        )),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30),
+                    child: TextField(
+                        controller: name,
+                        decoration: InputDecoration(
+                          suffixIcon: const Row(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 10),
+                                child: Icon(
+                                  Icons.password_rounded,
+                                ),
+                              ),
+                              Text('Enter your Password ')
+                            ],
+                          ),
+                          filled: true,
+                          fillColor: const Color.fromARGB(255, 215, 215, 215),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20)),
+                        )),
+                  ),
+                ],
+              ),
             ));
   }
 }
