566fa84574c6ebf302526b785270b77a800e4808
Osamaalharbi9 <osamaalharbi.9900@gmail.com>
2023-09-25 23:49:05 +0300

    avatar

--- linktree/lib/main.dart
+++ linktree/lib/main.dart
@@ -2,4 +2,3 @@ import 'package:flutter/material.dart';
 import 'package:linktree/introduction_page/introduction_page.dart';
 
 void main()=>runApp( Introductionpage());
-
--- linktree/lib/main_page/main_page.dart
+++ linktree/lib/main_page/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Mainpage extends StatefulWidget {
   const Mainpage({super.key, required this.name1, required this.username1});
@@ -12,10 +13,29 @@ class _MainpageState extends State<Mainpage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [Text(widget.name1),
-          Text(widget.username1),
-        ],
+      body: SizedBox.expand(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 140,
+            ),
+            const CircleAvatar(
+              foregroundImage: AssetImage('assets/images/vecteezy_default-profile-account-unknown-icon-black-silhouette_20765399.jpg')
+                  ,
+              radius: 60,
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Text(
+              widget.name1,
+              style:
+                  GoogleFonts.inter(fontSize: 20, fontWeight: FontWeight.w500),
+            ),
+            Text('@${widget.username1}'),
+          ],
+        ),
       ),
     );
   }
